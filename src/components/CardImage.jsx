077e8443e81f3fbf416d0bafa36d9f30aa0077bd
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-15 13:18:08 -0600

    Add CardImage component

---
+++ src/components/CardImage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+
+
+export default CardImage = ( {item} ) => {
+  return (
+    <View>
+      <Image source={{uri: item.img}} alt={item.title} style={styles.image}></Image>
+      <View style={styles.containerStar}>
+        <FontAwesomeIcon icon={faStar} size={12} color='#EEBA00'/>
+        <Text style={styles.points}>{item.calification}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'cover',
+    width: 110,
+    height: 115,
+    borderRadius: 10,
+    marginRight: 14
+  },
+  containerStar: {
+    flexDirection: 'row',
+    position: 'absolute',
+    backgroundColor: '#FBEDB7',
+    borderRadius: 12,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    top: 86,
+    left: 30,
+  },
+  points: {
+    marginLeft: 5,
+    fontSize: 12,
+    fontWeight: 'bold'
+  },
+})
