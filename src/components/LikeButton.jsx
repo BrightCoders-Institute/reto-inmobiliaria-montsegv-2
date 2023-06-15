c295ecca7efb7d4a4b234589fb9af36937e7fe81
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-15 13:47:20 -0600

    Add like button component and create Home and card details screens

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View, StyleSheet } from 'react-native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+
+export default LikeButton = () => {
+  return (
+      <View style={styles.containerHeart}>
+        <FontAwesomeIcon icon={faHeart} size={14} color={'white'}/>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerHeart: {
+    alignSelf: "flex-end",
+    backgroundColor: '#00B074',
+    borderRadius: 150 / 2,
+    padding: 7,
+    top: 8,
+    left: -25,
+  }
+})
