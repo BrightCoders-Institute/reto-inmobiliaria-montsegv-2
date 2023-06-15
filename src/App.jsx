4741d835e4c135f0c72a15601a3034c1821b031a
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-15 12:59:29 -0600

    Change information in the apartments.json and add the structurefolders

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native-safe-area-context';
+
+function App() {
+  return (
+    <SafeAreaView>
+     <Text>Inmobiliaria</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App;
